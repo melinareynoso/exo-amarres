c4fe8a0d8a61aa50ee67af9d0321ac3adc2b65de
Melina <melina.reynoso@hotmail.com>
2023-02-15 13:26:50 -0300

    Revert "navbar"
    This reverts commit bfb6ad9ad8d5ba80de4c1306f67a775f30c61f62.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,20 +1,18 @@
 import logo from "../../images/logo.JPG";
-import "./Navbar.css";
 
-export default function Navbar() {
-  return (
-    <nav class="navbar">
-      <div id="left"></div>
-      <div id="center">
-        <img id="logo" alt="logo" src={logo} />
-      </div>
-      <div id="rigth">
-        <ul>
-          <li>Inicio</li>
-          <li>Que hacemos</li>
-          <li>Contactanos</li>
-        </ul>
-      </div>
-    </nav>
-  );
+
+
+export default function Navbar(){
+    return (
+        <navbar class="navbar">
+            <div id="left"></div>
+            <div id="center"><img alt="logo" src={logo}/></div>
+            <div id="rigth"><ul>
+                <li>Inicio</li>
+                <li>Que hacemos</li>
+                <li>Contactanos</li>
+            </ul></div>
+        </navbar>
+        )
 }
+
