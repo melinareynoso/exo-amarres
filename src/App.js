19a41120d17e7eb2ea716871e715277cc6a7695c
Lean <lean@lean.com>
2023-03-03 03:12:20 -0300

    3/3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Services from './Components/Servicios/Servicios';
+import Especialistas from './Components/Especialistas/Especialistas';
+import Testimonios from './Components/Testimonios/Testimonios';
 import { FaWhatsapp } from "react-icons/fa";
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      {/* este div lo agregue para no meter todas las secciones juntas en un componente
-      para que puedan comapartir el mismo background todas */}
-      {/* <div className="fondo"> */}
-        <Services/>
-      {/* </div> */}
-      <a
-        className="btn-wsp"
+      <a className="btn-wsp"
         rel="noopener noreferrer"
         href="https://api.whatsapp.com/send?phone=5493541392076"
-        target="_blank"
-      >
+        target="_blank">
         <FaWhatsapp id="whatsapp-icon-float" />
       </a>
+      <Header/>
+      <Services/>
+      <Especialistas/>
+      <Testimonios/>
+
+      
+      
 
     </div>
   );
---
+++ src/Components/Testimonios/Testimonios.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Testimonios.css'
+import Carousel from "./Carousel";
+
+
+
+
+export default function Testimonios () { 
+    
+    return (
+        <section id="testimonios">
+            <h2>Testimonios</h2>
+            <div className="container-carousel">
+            </div>
+        </section>
+    )
+
+}
