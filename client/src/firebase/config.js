9cabc678223d981d76193f6aedf5c13513bddf02
Lean <lean@lean.com>
2023-03-13 21:44:54 -0300

    probando git ignore config.js

---
+++ client/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDysoqiDHnQTKgUYTIa6umrZa34SMVjvis",
+  authDomain: "exoamarres.firebaseapp.com",
+  projectId: "exoamarres",
+  storageBucket: "exoamarres.appspot.com",
+  messagingSenderId: "516901394652",
+  appId: "1:516901394652:web:23470aec2a0ce9dbb73c19"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
